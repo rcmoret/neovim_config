f356276adba54d563d3747c09e1f6346b89466cb
RyanMoret <ryancmoret@gmail.com>
2024-06-04 13:30:14 -0500

    Modified treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,23 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    })
-  end,
+  -- opts = function(_, opts)
+  --   opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  --     "lua",
+  --     "vim",
+  --     -- add more arguments for adding more treesitter parsers
+  --   })
+  -- end,
+  opts = {
+    indent = {
+      enable = true,
+      disable = {"ruby"},
+    },
+    autotag = {
+      enable = true,
+      rename = true,
+    },
+  },
 }
