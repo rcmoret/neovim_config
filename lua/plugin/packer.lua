c582be174aaba250d69b6853fd5144d4d8756b44
RyanMoret <ryancmoret@gmail.com>
2024-06-08 14:35:18 -0500

    Add tmux navigation

--- lua/plugin/packer.lua
+++ lua/plugin/packer.lua
@@ -15,4 +15,21 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     requires = { {'nvim-lua/plenary.nvim'} }
   })
+
+  -- use "alexghergh/nvim-tmux-navigation"
+  use({
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      require"nvim-tmux-navigation".setup {
+          disable_when_zoomed = true, -- defaults to false
+          keybindings = {
+              left = "<C-h>",
+              down = "<C-j>",
+              up = "<C-k>",
+              right = "<C-l>",
+              last_active = "<C-\\>",
+              next = "<C-Space>",
+          }
+      }
+    end})
 end)
