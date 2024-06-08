f4de8f2e38959a04fad65ee350f10088b6d4b23f
RyanMoret <ryancmoret@gmail.com>
2024-06-08 13:44:24 -0500

    Installed packer

---
+++ after/plugin/packer.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "<C-s>", "<cmd>PackerSync<CR>", { desc = "packer sync" })
+vim.keymap.set("n", "<Leader>kc", "<cmd>PackerClean<CR>", { desc = "pac[k]er [c]lean" })
+vim.keymap.set("n", "<Leader>kC", "<cmd>PackerCompile<CR>", { desc = "pac[k]er [C]ompile" })
+vim.keymap.set("n", "<Leader>ki", "<cmd>PackerInstall<CR>", { desc = "pac[k]er [i]nstall" })
+vim.keymap.set("n", "<Leader>ks", "<cmd>PackerSync<CR>", { desc = "pac[k]er [s]ync" })
+vim.keymap.set("n", "<Leader>kS", "<cmd>PackerStatus<CR>", { desc = "pac[k]er [S]tatus" })
