bd9d5b0ce1d5202d709a3b9ef4e8aa1c115dfb88
RyanMoret <ryancmoret@gmail.com>
2024-06-08 23:44:18 -0500

    Which-key

---
+++ lua/plugin/which_key.lua
@@ -0,0 +1,26 @@
+local wk = require("which-key")
+
+wk.setup({
+  plugins = {
+    marks = false,
+    registers = false,
+  }
+})
+
+wk.register({
+  f = {
+    name = "File helpers",
+    b = "Find Buffers",
+    h = "Find Help Tags",
+    g = "Live Git Grep",
+    f = "Find Files",
+  },
+  c = { name = "Copy to Clipboard" },
+  p = { name = "Paste from Clipboard" },
+  P = {
+    name = "Packer",
+    s = { name = "Packer Sync" },
+  },
+  m = { name = "Mason Plugins" },
+  x = { name = "Cut to Clipboard" },
+}, { prefix = "<Leader>" })
