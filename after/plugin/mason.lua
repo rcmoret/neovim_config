82872d31d9f5f4267333d2ec61beefebae912509
RyanMoret <ryancmoret@gmail.com>
2024-06-10 08:02:28 -0500

    LSP setup

---
+++ after/plugin/mason.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<Leader>mp", "<cmd>Mason<CR>", { desc = "[m]ason [p]lugin manager" })
+vim.keymap.set("n", "<Leader>mu", "<cmd>MasonUpdate<CR>", { desc = "[m]ason [u]pdate plugins" })
--- lua/plugin/which_key.lua
+++ lua/plugin/which_key.lua
@@ -12,12 +12,11 @@ wk.setup({
 
 wk.register({
   c = { name = "[c]opy to clipboard" },
-  g = { name = "Git helpers", },
-  m = { name = "Mason Plugins" },
+  g = { name = "[g]it helpers", },
   l = { name = "[l]sp commands" },
+  m = { name = "[m]ason plugins" },
   p = { name = "[p]aste from clipboard" },
   P = { name = "[P]acker" },
-  m = { name = "[m]ason plugins" },
   s = { name = "[s]ubstitute current word (line)" },
   S = { name = "[S]ubstitute current word (file)" },
   t = { name = "[t]elescope search" },
