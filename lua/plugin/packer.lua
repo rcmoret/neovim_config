a2484e84caa75d9eae4617f8d4034729d76d0969
RyanMoret <ryancmoret@gmail.com>
2024-06-13 14:48:42 -0500

    tpope/vim-surround

--- lua/plugin/packer.lua
+++ lua/plugin/packer.lua
@@ -51,10 +51,11 @@ return require('packer').startup(function(use)
   use "tpope/vim-commentary"
   use "tpope/vim-fugitive"
   use "tpope/vim-rhubarb"
+  use "tpope/vim-rails"
+  use "tpope/vim-surround"
   use "lukas-reineke/indent-blankline.nvim"
   use "lewis6991/gitsigns.nvim"
   use "mileszs/ack.vim"
-  use "tpope/vim-rails"
   use "mbbill/undotree"
   use "folke/which-key.nvim"
   use {
