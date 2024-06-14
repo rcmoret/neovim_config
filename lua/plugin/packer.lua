e2a3202d1c12b48d931dc2ee28a2cfc1eae79c87
RyanMoret <ryancmoret@gmail.com>
2024-06-13 21:40:06 -0500

    Add tpope/endwise

--- lua/plugin/packer.lua
+++ lua/plugin/packer.lua
@@ -67,6 +67,7 @@ use {
   use "tpope/vim-rails"
   use "tpope/vim-surround"
   use "tpope/vim-abolish"
+  use "tpope/vim-endwise"
   use "lukas-reineke/indent-blankline.nvim"
   use "lewis6991/gitsigns.nvim"
   use "mileszs/ack.vim"
