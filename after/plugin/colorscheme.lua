b496743f9965a41b76897dd67e353e77ed58a0cf
RyanMoret <ryancmoret@gmail.com>
2024-07-04 08:08:16 -0500

    background, bluloco colorscheme

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,1 +1,8 @@
 vim.cmd.colorscheme("bluloco")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "#000018" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.opt.cursorline = true
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#adb5bd" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffd60a", bold = true })
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
