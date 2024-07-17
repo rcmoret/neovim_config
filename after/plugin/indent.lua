6ffae477dc1080595fed083a0aa5206a74ee542f
RyanMoret <ryancmoret@gmail.com>
2024-07-16 21:55:11 -0500

    Consolidate ui elements

--- after/plugin/indent.lua
+++
@@ -1,7 +0,0 @@
-require("ibl").setup({
-  scope = {
-    enabled = false
-  }
-})
-
-vim.api.nvim_set_hl(0, "@ibl.indent.char.1", { fg = "#313b4e" })
--- after/plugin/ui.lua
+++ after/plugin/ui.lua
@@ -1,3 +1,11 @@
+require("ibl").setup({
+  scope = {
+    enabled = false
+  }
+})
+
+vim.api.nvim_set_hl(0, "@ibl.indent.char.1", { fg = "#313b4e" })
+
 local noice = require("noice")
 
 local icons = require("config.icons").icons
